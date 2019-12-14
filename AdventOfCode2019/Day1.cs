4faddc45d42ed7bc3dee65bf2a93f55dda031299
mdk <mdk@lostindetails.com>
2019-12-14 23:21:26 +0100

    initial commit

---
+++ AdventOfCode2019/Day1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode2019
+{
+  public class Day1
+  {
+    static double GetFuel(double mass)
+      => Math.Floor(mass / 3d) - 2;
+
+    [Theory,
+      InlineData(12, 2),
+      InlineData(14, 2),
+      InlineData(1969, 654),
+      InlineData(100756, 33583)
+      ]
+    public void Day1_FuelForMass(double mass, double expectedFuel)
+    {
+      var fuel = GetFuel(mass);
+
+      fuel.Should().Be(expectedFuel);
+    }
+
+    readonly double[] _masses = new double[]
+      {
+        75592
+,56081
+,141375
+,103651
+,132375
+,90584
+,94148
+,85029
+,95082
+,148499
+,108192
+,97739
+,60599
+,140308
+,125171
+,129160
+,143118
+,98762
+,103907
+,115389
+,127835
+,57917
+,72980
+,88747
+,86595
+,130407
+,116862
+,84652
+,112817
+,136922
+,51900
+,76677
+,146244
+,121897
+,99310
+,136486
+,84665
+,117344
+,88992
+,83929
+,74820
+,56651
+,74001
+,88636
+,51232
+,57878
+,114559
+,58879
+,145519
+,83727
+,111774
+,146256
+,123479
+,86955
+,64027
+,59812
+,59211
+,85835
+,58084
+,113676
+,119161
+,106368
+,137358
+,85290
+,81131
+,124857
+,51759
+,82977
+,138957
+,146216
+,147807
+,72265
+,60332
+,136741
+,110215
+,89293
+,148703
+,73152
+,93080
+,140220
+,68511
+,77397
+,51934
+,100243
+,92442
+,135254
+,98873
+,51105
+,118755
+,79155
+,89249
+,137430
+,142807
+,86334
+,117266
+,149484
+,89284
+,63361
+,52269
+,111666
+      };
+
+    [Fact]
+    public void Day1_Sum()
+    {
+      var sum = this._masses.Select(m => GetFuel(m)).Sum();
+
+      Console.WriteLine($"sum of all masses: {sum}");
+    }
+
+    static double GetFuelRec(double mass, double acc)
+    {
+      var m = GetFuel(mass);
+      if (m > 0)
+        return GetFuelRec(m, acc + m);
+      else
+        return acc;
+    }
+
+    [Theory,
+      InlineData(14, 2),
+      InlineData(100756, 50346),
+      ]
+    public void Day1_FuelForMassRec(double mass, double expectedFuel)
+    {
+      var fuel = GetFuelRec(mass, 0);
+
+      fuel.Should().Be(expectedFuel);
+    }
+
+    [Fact]
+    public void Day1_SumRec()
+    {
+      var sum = this._masses.Select(m => GetFuelRec(m, 0)).Sum();
+
+      Console.WriteLine($"sum of all masses taking into account recursive fuel: {sum}");
+    }
+  }
+}
---
+++ AdventOfCode2019/Day2.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode2019
+{
+  public class Day2
+  {
+    enum OpCodes
+    {
+      Add      = 1,
+      Multiply = 2,
+      End      = 99,
+    }
+
+    static (int[] State, Index Index) CalcOp(int[] stateIn, Index i)
+    {
+      var stateOut = stateIn.ToArray();
+
+      int GetAddress(int i)
+        => stateIn[i];
+
+      int GetValue(int i)
+        => stateIn[GetAddress(i)];
+
+      OpCodes op = (OpCodes)stateIn[i];
+      var v1 = GetValue(i.Value + 1);
+      var v2 = GetValue(i.Value + 2);
+      var outIndex = GetAddress(i.Value + 3);
+
+      var result =
+        op switch
+        {
+          OpCodes.Add => v1 + v2,
+          OpCodes.Multiply => v1 * v2,
+          _ => throw new InvalidOperationException($"unable to handle OpCode: {op}"),
+        };
+      stateOut[outIndex] = result;
+
+      return (State: stateOut, Index: new Index(i.Value + 4));
+    }
+
+    int[] Run(int[] program)
+    {
+      var state = program.ToArray();
+      Index i = 0;
+
+      bool exit = false;
+      while (true)
+      {
+        var op = (OpCodes)state[i];
+
+        switch (op)
+        {
+          case OpCodes.Add:
+          case OpCodes.Multiply:
+            (state, i) = CalcOp(state, i);
+            break;
+
+          case OpCodes.End: exit = true;  break;
+          default:
+            throw new InvalidOperationException($"Invalid opcode: {op} at index: {i}");
+        }
+
+        if (exit)
+          break;
+      }
+
+      return state;
+    }
+
+    string FormatState(int[] state)
+    {
+      var builder = new StringBuilder();
+      for (int i = 0; i < state.Length - 1; i++)
+        builder.Append($"{state[i]},");
+      builder.Append($"{state[state.Length-1]}");
+      return builder.ToString();
+    }
+
+    [Theory,
+      InlineData(new int[] { 1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50 },
+                 new int[] { 3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50 }),
+      InlineData(new int[] { 1, 0, 0, 0, 99 }, new int[] { 2, 0, 0, 0, 99 }),
+      InlineData(new int[] { 2, 3, 0, 3, 99 }, new int[] { 2, 3, 0, 6, 99 }),
+      InlineData(new int[] { 2, 4, 4, 5, 99, 0 }, new int[] { 2, 4, 4, 5, 99, 9801 }),
+      InlineData(new int[] { 1, 1, 1, 4, 99, 5, 6, 0, 99 }, new int[] { 30, 1, 1, 4, 2, 5, 6, 0, 99 }),
+      ]
+    public void Day2_Run(int[] program, int[] resultState)
+    {
+      Console.WriteLine(FormatState(program));
+
+      /* act */
+      var state = Run(program);
+
+      Console.WriteLine(FormatState(state));
+
+      /* assert */
+      state.Should().BeEquivalentTo(resultState);
+    }
+
+    static int[] GetInput()
+      => new int[] { 1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,6,19,23,1,23,13,27,2,6,27,31,1,5,31,35,2,10,35,39,1,6,39,43,1,13,43,47,2,47,6,51,1,51,5,55,1,55,6,59,2,59,10,63,1,63,6,67,2,67,10,71,1,71,9,75,2,75,10,79,1,79,5,83,2,10,83,87,1,87,6,91,2,9,91,95,1,95,5,99,1,5,99,103,1,103,10,107,1,9,107,111,1,6,111,115,1,115,5,119,1,10,119,123,2,6,123,127,2,127,6,131,1,131,2,135,1,10,135,0,99,2,0,14,0 };
+
+    [Fact]
+    public void Day2_Test()
+    {
+      var program = GetInput();
+      program[1] = 12;
+      program[2] = 2;
+
+      var state = this.Run(program);
+
+      Console.WriteLine(FormatState(state));
+    }
+  }
+}
